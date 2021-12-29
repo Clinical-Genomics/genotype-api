7547519b204bc63867aa1c591ab5c6d0ddfb2502
mayabrandi <maya.brandi@scilifelab.se>
2021-12-29 13:28:46 +0100

    cleaning sequence endpoint

--- genotype_api/api/endpoints/sequences.py
+++ genotype_api/api/endpoints/sequences.py
@@ -1,10 +1,9 @@
 """Routes for uploading sequence analyses"""
 
-from io import BytesIO
 from pathlib import Path
 from typing import List
 
-import genotype_api.crud.analyses
+from genotype_api.crud.analyses import get_analysis_type_sample, create_analysis
 from genotype_api.crud.samples import get_sample, create_sample
 from fastapi import APIRouter, Depends, File, HTTPException, UploadFile, status
 from genotype_api.database import get_session
@@ -15,28 +14,34 @@ from sqlmodel import Session
 router = APIRouter()
 
 
-@router.post("/", response_model=List[Analysis])
-def upload_vcf(file: UploadFile = File(...), session: Session = Depends(get_session)):
-    file_name: Path = Path(file.filename)
+def check_vcf_file(file_path: str) -> Path:
+    """Check file and file name"""
+
+    file_name: Path = Path(file_path)
     if not file_name.name.endswith(".vcf"):
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Please select a vcf file for upload",
         )
-    content = file.file.read().decode("utf-8")
+    return file_name
 
+
+@router.post("/", response_model=List[Analysis])
+def upload_sequence_analysis(file: UploadFile = File(...), session: Session = Depends(get_session)):
+    """Reading vcf file, creating and uploading analysis and sample objects to db"""
+
+    file_name: Path = check_vcf_file(file_path=file.filename)
+    content = file.file.read().decode("utf-8")
     sequence_analysis = SequenceAnalysis(vcf_file=content, source=str(file_name))
     analyses = []
     for analysis_obj in sequence_analysis.generate_analyses():
-        db_analysis = genotype_api.crud.analyses.get_analysis_type_sample(
+        db_analysis = get_analysis_type_sample(
             session=session, sample_id=analysis_obj.sample_id, analysis_type="sequence"
         )
         if db_analysis:
             raise HTTPException(status_code=400, detail="Analysis already exists")
         if not get_sample(session=session, sample_id=analysis_obj.sample_id):
             create_sample(session=session, sample=Sample(id=analysis_obj.sample_id))
-        analyses.append(
-            genotype_api.crud.analyses.create_analysis(session=session, analysis=analysis_obj)
-        )
+        analyses.append(create_analysis(session=session, analysis=analysis_obj))
 
     return analyses
