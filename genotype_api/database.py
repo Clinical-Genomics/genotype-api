f07aecbfd84da97a28652810679a1e2e7f984626
Maria Ropat <35531751+Mropat@users.noreply.github.com>
2022-03-22 15:59:59 +0100

    Session BrokenPipe handling (#48)(patch)
    ### Fixed
    * Modify session flags to remove 500 error in stale threads

--- genotype_api/database.py
+++ genotype_api/database.py
@@ -6,7 +6,7 @@ from genotype_api.config import settings
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
-engine = create_engine(settings.db_uri)
+engine = create_engine(settings.db_uri, pool_pre_ping=True)
 
 
 def get_session():
