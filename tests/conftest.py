81d49d8037f9835376de0e8dcba3185fbf85a140
Vincent Janvid <vincent.janvid@scilifelab.se>
2023-06-16 09:41:52 +0200

    dummy fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -27,3 +27,8 @@ def fixture_include_key() -> str:
 def fixture_sample_name() -> str:
     """Return a sample name"""
     return "sample"
+
+@pytest.fixture(name="sample_id")
+def fixture_sample_id() -> str:
+    """Return a sample name"""
+    return "sample_id"
