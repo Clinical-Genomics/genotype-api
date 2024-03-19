7f66905bb49aaf5e0138b32448d3aa7f3d40e149
Christian Oertlin <c.oertlin@gmail.com>
2024-03-19 14:45:37 +0100

    fix dict

--- genotype_api/services/analysis_service/analysis_service.py
+++ genotype_api/services/analysis_service/analysis_service.py
@@ -8,9 +8,11 @@ from genotype_api.dto.analysis import AnalysisWithGenotypeResponse
 
 
 class AnalysisService:
+    """This service acts as a translational layer between the crud and the api."""
+
     def __init__(self, session: Session):
         self.session: Session = session
 
     def get_analysis_with_genotype_response(self, analysis_id: int) -> AnalysisWithGenotypeResponse:
         analysis: Analysis = get_analysis_by_id(session=self.session, analysis_id=analysis_id)
-        return AnalysisWithGenotypeResponse(analysis.dict())
+        return AnalysisWithGenotypeResponse(**analysis.dict())
