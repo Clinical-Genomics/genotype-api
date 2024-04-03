65d47ac9164c26c2287efd9e14399911e6fa6f7d
Christian Oertlin <c.oertlin@gmail.com>
2024-04-03 12:22:21 +0200

    remove more unused models

--- genotype_api/dto/dto.py
+++ genotype_api/dto/dto.py
@@ -1,11 +1,4 @@
-from pydantic import constr
-
-
 from genotype_api.database import models
-
-
-class SNPRead(models.SNPBase):
-    id: constr(max_length=32)
 
 
 class PlateCreate(models.PlateBase):
