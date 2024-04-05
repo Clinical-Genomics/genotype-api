439eae85f7d74a1ae3022eef197bf43850d6f8be
Christian Oertlin <c.oertlin@gmail.com>
2024-04-05 13:03:45 +0200

    fix token info extraction

--- genotype_api/security.py
+++ genotype_api/security.py
@@ -2,31 +2,29 @@ import requests
 from fastapi import Depends, HTTPException, Security
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from jose import jwt
-from sqlalchemy.orm import Session
-
 from starlette import status
 from starlette.requests import Request
 
 from genotype_api.config import security_settings
 from genotype_api.database.models import User
-from genotype_api.database.database import get_session
 from genotype_api.database.store import get_store, Store
 from genotype_api.dto.user import CurrentUser
 
 
 def decode_id_token(token: str):
-    payload = jwt.decode(
-        token,
-        key=requests.get(security_settings.jwks_uri).json(),
-        algorithms=[security_settings.algorithm],
-        audience=security_settings.client_id,
-        options={
-            "verify_at_hash": False,
-        },
-    )
-    if not payload:
-        return jwt.get_unverified_claims(token)
-    return payload
+    try:
+        payload = jwt.decode(
+            token,
+            key=requests.get(security_settings.jwks_uri).json(),
+            algorithms=[security_settings.algorithm],
+            audience=security_settings.client_id,
+            options={
+                "verify_at_hash": False,
+            },
+        )
+        return payload
+    except jwt.JWTError:
+        return None
 
 
 class JWTBearer(HTTPBearer):
@@ -45,14 +43,17 @@ class JWTBearer(HTTPBearer):
                 status_code=status.HTTP_403_FORBIDDEN,
                 detail="Invalid authentication scheme.",
             )
-        self.verify_jwt(credentials.credentials)
-
-        return credentials.credentials
+        payload = self.verify_jwt(credentials.credentials)
+        return {"token": credentials.credentials, "payload": payload}
 
     def verify_jwt(self, jwtoken: str) -> dict | None:
         try:
-            return decode_id_token(jwtoken)
-        except Exception:
+            payload = decode_id_token(jwtoken)
+            if payload and "email" in payload and "username" in payload:
+                return {"email": payload["email"], "username": payload["username"]}
+            else:
+                return None
+        except jwt.JWTError:
             raise HTTPException(
                 status_code=status.HTTP_403_FORBIDDEN,
                 detail="Invalid token or expired token.",
@@ -63,12 +64,13 @@ jwt_scheme = JWTBearer()
 
 
 async def get_active_user(
-    token: str = Security(jwt_scheme),
+    token_info: dict = Security(jwt_scheme),
     store: Store = Depends(get_store),
 ):
     """Dependency for secure endpoints"""
-    payload: dict = decode_id_token(token)
-    user = CurrentUser(name=payload.get("username"), email=payload.get("email"))
+    user_email = token_info["payload"]["email"]
+    username = token_info["payload"]["username"]
+    user = CurrentUser(name=username, email=user_email)
     db_user: User = store.get_user_by_email(email=user.email)
     if not db_user:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User not in DB")
