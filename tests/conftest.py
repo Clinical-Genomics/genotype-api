bccd87dab0aec9a05f00524e37e9ff251c1c0b2f
ChristianOertlin <c.oertlin@gmail.com>
2024-04-08 15:17:25 +0200

    add(storehelpers and tests) (#127) (patch)
    # Description 
    
    add store helpers and tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,29 +1,118 @@
 """General fixtures"""
 
+import datetime
 from pathlib import Path
+from typing import Generator
 
 import pytest
+
+from genotype_api.database.database import initialise_database, create_all_tables, drop_all_tables
+from genotype_api.database.models import User, Plate, SNP, Sample, Genotype, Analysis
+from genotype_api.database.store import Store
+from tests.store_helpers import StoreHelpers
+
+
+@pytest.fixture
+def timestamp_now() -> datetime:
+    return datetime.datetime.now()
+
+
+@pytest.fixture
+def store() -> Generator[Store, None, None]:
+    """Return a CG store."""
+    initialise_database("sqlite:///")
+    _store = Store()
+    create_all_tables()
+    yield _store
+    drop_all_tables()
+
+
+@pytest.fixture
+def helpers(store: Store):
+    return StoreHelpers()
+
+
+@pytest.fixture
+def test_user() -> User:
+    return User(id=1, email="test@tester.com", name="Test Testorus")
+
+
+@pytest.fixture
+def test_plate(test_user: User, timestamp_now: datetime) -> Plate:
+    return Plate(
+        id=1,
+        plate_id="ID_1",
+        signed_by=test_user.id,
+        method_document="mdoc",
+        method_version="mdoc_ver",
+        created_at=timestamp_now,
+        signed_at=timestamp_now,
+    )
+
+
+@pytest.fixture
+def test_snp() -> SNP:
+    return SNP(id=1, ref="A", chrom="2", pos=12341)
+
+
+@pytest.fixture
+def test_sample_id() -> str:
+    return "test_sample"
+
+
+@pytest.fixture
+def sex_male() -> str:
+    return "male"
+
+
+@pytest.fixture
+def test_sample(timestamp_now: datetime, test_sample_id: str, sex_male: str) -> Sample:
+    return Sample(
+        id=test_sample_id,
+        status="test_status",
+        comment="test_comment",
+        sex=sex_male,
+        created_at=timestamp_now,
+    )
+
+
+@pytest.fixture
+def test_genotype() -> Genotype:
+    return Genotype(id=1, rsnumber="12315", analysis_id=1, allele_1="A", allele_2="G")
+
+
+@pytest.fixture
+def test_analysis(sex_male, timestamp_now: datetime, test_sample_id: str) -> Analysis:
+    return Analysis(
+        id=1,
+        type="genotype",
+        source="source",
+        sex=sex_male,
+        created_at=timestamp_now,
+        sample_id=test_sample_id,
+        plate_id=1,
+    )
 
 
 @pytest.fixture(name="fixtures_dir")
 def fixture_fixtures_dir() -> Path:
-    """Return the path to fixtures dir"""
+    """Return the path to fixtures dir."""
     return Path("tests/fixtures")
 
 
 @pytest.fixture(name="excel_file")
 def fixture_excel_file(fixtures_dir: Path) -> Path:
-    """Return the path to an excel file"""
+    """Return the path to an excel file."""
     return fixtures_dir / "excel" / "genotype.xlsx"
 
 
 @pytest.fixture(name="include_key")
 def fixture_include_key() -> str:
-    """Return the include key"""
+    """Return the include key."""
     return "-CG-"
 
 
 @pytest.fixture(name="sample_name")
 def fixture_sample_name() -> str:
-    """Return a sample name"""
+    """Return a sample name."""
     return "sample"
---
+++ tests/store_helpers.py
@@ -0,0 +1,74 @@
+"""Module that holds the store helper to be used to setup the testing environment."""
+
+from genotype_api.database.models import Sample, Analysis, User, SNP, Plate, Genotype
+from genotype_api.database.store import Store
+
+
+class StoreHelpers:
+    """Class to hold helper functions to setup the store testing environment."""
+
+    @classmethod
+    def add_entity(cls, store: Store, entity: Sample | Analysis | SNP | Plate | User | Genotype):
+        store.session.add(entity)
+
+    def ensure_sample(self, store: Store, sample: Sample, analyses: list[Analysis] = None):
+        """Add a sample to the store. Ensure its analyses are present."""
+        if analyses:
+            for analysis in analyses:
+                if not store.get_analysis_by_id(analysis.id):
+                    self.add_entity(store=store, entity=analysis)
+        self.add_entity(store=store, entity=sample)
+        store.session.commit()
+
+    def ensure_snp(self, store: Store, snp: SNP):
+        self.add_entity(store=store, entity=snp)
+        store.session.commit()
+
+    def ensure_plate(
+        self, store: Store, plate: Plate, analyses: list[Analysis] = None, user: User = None
+    ):
+        """Add a plate to the store ensure the associated user and analyses are present."""
+        if user and not store.get_user_by_email(user.email):
+            self.add_entity(store=store, entity=user)
+        if analyses:
+            for analysis in analyses:
+                if not store.get_analysis_by_id(analysis.id):
+                    self.add_entity(store=store, entity=analysis)
+        self.add_entity(store=store, entity=plate)
+        store.session.commit()
+
+    def ensure_user(self, store: Store, user: User, plates: list[Plate] = None):
+        """Add a user to the store and ensure the associated plates are present."""
+        if plates:
+            for plate in plates:
+                if not store.get_plate_by_id(plate.id):
+                    self.add_entity(store=store, entity=plate)
+        self.add_entity(store=store, entity=user)
+        store.session.commit()
+
+    def ensure_analysis(
+        self,
+        store: Store,
+        analysis: Analysis,
+        sample: Sample,
+        plate: Plate,
+        genotypes: list[Genotype],
+    ):
+        """Add an analysis to the store and ensure the associated sample, plate and genotypes are present."""
+        if sample and not store.get_sample(sample.id):
+            self.add_entity(store=store, entity=sample)
+        if plate and not store.get_plate_by_id(plate.id):
+            self.add_entity(store=store, entity=plate)
+        if genotypes:
+            for genotype in genotypes:
+                if not store.get_genotype_by_id(genotype.id):
+                    self.add_entity(store=store, entity=genotype)
+        self.add_entity(store=store, entity=analysis)
+        store.session.commit()
+
+    def ensure_genotype(self, store: Store, genotype: Genotype, analysis: Analysis = None):
+        """Add a genotype to the database and ensure the associated analysis is present."""
+        if analysis and not store.get_analysis_by_id(analysis.id):
+            self.add_entity(store=store, entity=analysis)
+        self.add_entity(store=store, entity=genotype)
+        store.session.commit()
