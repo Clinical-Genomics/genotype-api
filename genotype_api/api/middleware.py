83480b7933feed92e06e31fb836c7485120f203b
seallard <sebastian.allard@scilifelab.se>
2024-04-12 11:53:52 +0200

    Simplify middleware

--- genotype_api/api/middleware.py
+++ genotype_api/api/middleware.py
@@ -1,11 +1,13 @@
 from fastapi import Request
+from starlette.middleware.base import BaseHTTPMiddleware
 from genotype_api.database.database import close_session
 
 
-class DBSessionMiddleware:
-    async def __call__(self, request: Request, call_next):
-        try:
-            response = await call_next(request)
-        finally:
-            close_session()
+class DBSessionMiddleware(BaseHTTPMiddleware):
+    def __init__(self, app):
+        super().__init__(app)
+
+    async def dispatch(self, request: Request, call_next):
+        response = await call_next(request)
+        close_session()
         return response
