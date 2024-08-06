87492adcbd68c7d6a296e965283bed5fc9313869
seallard <sebastian.allard@scilifelab.se>
2024-08-06 10:12:24 +0200

    logging

--- genotype_api/api/middleware.py
+++ genotype_api/api/middleware.py
@@ -1,15 +1,20 @@
 from fastapi import Request
 from starlette.middleware.base import BaseHTTPMiddleware
 from genotype_api.database.database import close_session
+import logging
 
+logger = logging.getLogger(__name__)
 
 class DBSessionMiddleware(BaseHTTPMiddleware):
     def __init__(self, app):
+        logger.info("Initialising DBSessionMiddleware")
         super().__init__(app)
 
     async def dispatch(self, request: Request, call_next):
         try:
+            logger.info("In dispatch")
             response = await call_next(request)
         finally:
+            logger.info("Closing session")
             close_session()
         return response
