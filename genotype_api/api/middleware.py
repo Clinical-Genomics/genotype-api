e1929a0b7d8b607f5bda0c1c65b792236dd5bcad
seallard <sebastian.allard@scilifelab.se>
2024-08-06 10:13:50 +0200

    logging

--- genotype_api/api/middleware.py
+++ genotype_api/api/middleware.py
@@ -5,6 +5,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 class DBSessionMiddleware(BaseHTTPMiddleware):
     def __init__(self, app):
         logger.info("Initialising DBSessionMiddleware")
