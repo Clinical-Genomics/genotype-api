30ff74270e00eca7d99151d0113ff2aa121c7f0b
ahdamin <ahmed.amin@scilifelab.se>
2024-09-26 17:03:50 +0200

    Add error handling middleware

--- genotype_api/api/middleware.py
+++ genotype_api/api/middleware.py
@@ -1,8 +1,14 @@
+import logging
+
 from fastapi import Request
+from fastapi.responses import JSONResponse
+from sqlalchemy.exc import OperationalError, PendingRollbackError
 from starlette.middleware.base import BaseHTTPMiddleware
 
 from genotype_api.database.database import close_session, get_session
 from genotype_api.exceptions import GenotypeDBError
+
+LOG = logging.getLogger(__name__)
 
 
 class DBSessionMiddleware(BaseHTTPMiddleware):
@@ -10,13 +16,36 @@ class DBSessionMiddleware(BaseHTTPMiddleware):
         super().__init__(app)
 
     async def dispatch(self, request: Request, call_next):
+        session = None
+        error_message = JSONResponse(
+            status_code=500, content={"message": "Internal server error: database session error."}
+        )
+
         try:
-            response = await call_next(request)
-            return response
-        except Exception:
             session = get_session()
-            if session.is_active:
+            if session is None:
+                return error_message
+            elif session.dirty:
+                session.flush()
+            else:
+                response = await call_next(request)
+                return response
+
+        except PendingRollbackError as e:
+            if session and session.is_active:
                 session.rollback()
-            raise GenotypeDBError
+            LOG.debug(f"DB session error occurred: {e}")
+            return error_message
+
+        except OperationalError as e:
+            LOG.debug(f"Database connection lost: {e}")
+            return error_message
+
+        except Exception as e:
+            if session and session.is_active:
+                session.rollback()
+            LOG.debug(f"DB session occurred: {e}")
+            return error_message
         finally:
-            close_session()
+            if session:
+                close_session()
