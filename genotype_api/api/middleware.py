5d3d73153a66eb4840a7df60f803fb76d4049c67
ahdamin <ahmed.amin@scilifelab.se>
2024-09-26 11:56:18 +0200

    use GenotypeDBError

--- genotype_api/api/middleware.py
+++ genotype_api/api/middleware.py
@@ -1,7 +1,7 @@
 from fastapi import Request
 from starlette.middleware.base import BaseHTTPMiddleware
-
-from genotype_api.database.database import close_session, get_session
+from genotype_api.exceptions import GenotypeDBError
+from genotype_api.database.database import close_session
 
 
 class DBSessionMiddleware(BaseHTTPMiddleware):
@@ -11,11 +11,8 @@ class DBSessionMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         try:
             response = await call_next(request)
-        except Exception as e:
-            raise e
-        finally:
-            session = get_session()
-            if session.is_active:
-                session.rollback()
+        except Exception:
+            raise GenotypeDBError
+        finally
             close_session()
         return response
