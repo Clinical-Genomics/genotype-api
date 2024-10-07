1efeb54ff4a78fd0ca952dd30e80745844bb9219
ahdamin <ahmed.amin@scilifelab.se>
2024-10-07 21:58:29 +0200

    Add async support to services

--- genotype_api/services/endpoint_services/user_service.py
+++ genotype_api/services/endpoint_services/user_service.py
@@ -1,10 +1,12 @@
 """Module to holds the user service."""
 
 from pydantic import EmailStr
+
 from genotype_api.database.models import User
 from genotype_api.database.store import Store
-from genotype_api.dto.user import UserResponse, UserRequest, PlateOnUser
-from genotype_api.exceptions import UserNotFoundError, UserArchiveError, UserExistsError
+from genotype_api.dto.user import PlateOnUser, UserRequest, UserResponse
+from genotype_api.exceptions import (UserArchiveError, UserExistsError,
+                                     UserNotFoundError)
 from genotype_api.services.endpoint_services.base_service import BaseService
 
 
@@ -30,35 +32,35 @@ class UserService(BaseService):
         plates: list[PlateOnUser] = self._get_plates_on_user(user)
         return UserResponse(email=user.email, name=user.name, id=user.id, plates=plates)
 
-    def create_user(self, user: UserRequest):
-        existing_user: User = self.store.get_user_by_email(email=user.email)
+    async def create_user(self, user: UserRequest):
+        existing_user: User = await self.store.get_user_by_email(email=user.email)
         if existing_user:
             raise UserExistsError
         db_user = User(email=user.email, name=user.name)
-        new_user: User = self.store.create_user(user=db_user)
+        new_user: User = await self.store.create_user(user=db_user)
         return self._create_user_response(new_user)
 
-    def get_users(self, skip: int, limit: int) -> list[UserResponse]:
-        users: list[User] = self.store.get_users_with_skip_and_limit(skip=skip, limit=limit)
+    async def get_users(self, skip: int, limit: int) -> list[UserResponse]:
+        users: list[User] = await self.store.get_users_with_skip_and_limit(skip=skip, limit=limit)
         return [self._create_user_response(user) for user in users]
 
-    def get_user(self, user_id: int) -> UserResponse:
-        user: User = self.store.get_user_by_id(user_id=user_id)
+    async def get_user(self, user_id: int) -> UserResponse:
+        user: User = await self.store.get_user_by_id(user_id=user_id)
         if not user:
             raise UserNotFoundError
         return self._create_user_response(user)
 
-    def delete_user(self, user_id: int):
-        user: User = self.store.get_user_by_id(user_id=user_id)
+    async def delete_user(self, user_id: int):
+        user: User = await self.store.get_user_by_id(user_id=user_id)
         if not user:
             raise UserNotFoundError
         if user.plates:
             raise UserArchiveError
-        self.store.delete_user(user=user)
+        await self.store.delete_user(user=user)
 
-    def update_user_email(self, user_id: int, email: EmailStr):
-        user: User = self.store.get_user_by_id(user_id=user_id)
+    async def update_user_email(self, user_id: int, email: EmailStr):
+        user: User = await self.store.get_user_by_id(user_id=user_id)
         if not user:
             raise UserNotFoundError
-        user: User = self.store.update_user_email(user=user, email=email)
+        user: User = await self.store.update_user_email(user=user, email=email)
         return self._create_user_response(user)
