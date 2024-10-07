ea24e2f972b5261887cd8442cdd6c9d119128a6d
ahdamin <ahmed.amin@scilifelab.se>
2024-10-07 21:45:30 +0200

    Refactor BaseHandler for async queries

--- genotype_api/database/base_handler.py
+++ genotype_api/database/base_handler.py
@@ -1,6 +1,10 @@
 from dataclasses import dataclass
 from typing import Type
-from sqlalchemy.orm import Session, Query, DeclarativeBase
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from sqlalchemy.orm import DeclarativeBase, Query
+
 from genotype_api.database.models import Analysis, Sample
 
 
@@ -8,12 +12,12 @@ from genotype_api.database.models import Analysis, Sample
 class BaseHandler:
     """All queries in one base class."""
 
-    def __init__(self, session: Session):
+    def __init__(self, session: AsyncSession):
         self.session = session
 
     def _get_query(self, table: Type[DeclarativeBase]) -> Query:
         """Return a query for the given table."""
-        return self.session.query(table)
+        return select(table)
 
     def _get_join_analysis_on_sample(self) -> Query:
         return self._get_query(table=Sample).join(Analysis)
