96fea87e275a492b5698a22fb97cd8d2b90fe795
ahdamin <ahmed.amin@scilifelab.se>
2024-10-11 00:58:07 +0200

    Use select and fetch_all_rows

--- tests/database/crud/test_delete.py
+++ tests/database/crud/test_delete.py
@@ -1,5 +1,6 @@
 """Module to test the delete functionality of the genotype API CRUD."""
 
+from sqlalchemy.future import select
 from sqlalchemy.orm import Query
 
 from genotype_api.database.models import SNP, Analysis, Plate, Sample, User
@@ -21,52 +22,55 @@ async def test_delete_analysis(base_store: Store, test_analysis: Analysis):
 
 async def test_delete_sample(base_store: Store, test_sample: Sample):
     # GIVEN a sample and a store with the sample
-    query: Query = base_store._get_query(Sample)
-    result = await base_store.session.execute(query)
-    assert test_sample in result.scalars().all()
+    query: Query = select(Sample)
+    sample = await base_store.fetch_all_rows(query)
+    assert test_sample in sample
 
     # WHEN deleting the sample
     await base_store.delete_sample(sample=test_sample)
 
     # THEN the sample is deleted
-    result = await base_store.session.execute(base_store._get_query(test_sample))
-    assert test_sample not in result.scalars().all()
+    sample = await base_store.fetch_all_rows(query)
+    assert test_sample not in sample
 
 
 async def test_delete_plate(base_store: Store, test_plate: Plate):
     # GIVEN a plate and a store with the plate
-    result = await base_store.session.execute(base_store._get_query(test_plate))
-    assert test_plate in result.scalars().all()
+    query: Query = select(Plate)
+    plate = await base_store.fetch_all_rows(query)
+    assert test_plate in plate
 
     # WHEN deleting the plate
     await base_store.delete_plate(plate=test_plate)
 
     # THEN the plate is deleted
-    result = await base_store.session.execute(base_store._get_query(test_plate))
-    assert test_plate not in result.scalars().all()
+    plate = await base_store.fetch_all_rows(query)
+    assert test_plate not in plate
 
 
 async def test_delete_user(base_store: Store, test_user: User):
     # GIVEN a user and a store with the user
-    result = await base_store.session.execute(base_store._get_query(test_user))
-    assert test_user in result.scalars().all()
+    query: Query = select(User)
+    user = await base_store.fetch_all_rows(query)
+    assert test_user in user
 
     # WHEN deleting the user
     base_store.delete_user(user=test_user)
 
     # THEN the user is deleted
-    result = await base_store.session.execute(base_store._get_query(test_user))
-    assert test_user not in result.scalars().all()
+    user = await base_store.fetch_all_rows(query)
+    assert test_user not in user
 
 
 async def test_delete_snps(base_store: Store, test_snp: SNP):
     # GIVEN an SNP and a store with the SNP
-    result = await base_store.session.execute(base_store._get_query(test_snp))
-    assert result.scalars().all()
+    query: Query = select(SNP)
+    snp = await base_store.fetch_all_rows(query)
+    assert snp
 
     # WHEN deleting the SNP
     base_store.delete_snps()
 
     # THEN all SNPs are deleted
-    result = await base_store.session.execute(base_store._get_query(test_snp))
-    assert not result.scalars().all()
+    snp = await base_store.fetch_all_rows(query)
+    assert not snp
