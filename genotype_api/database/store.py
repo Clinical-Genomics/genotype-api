96abb8ffc61e6a1c88a8ffeee39f40b2ccab87ab
ahdamin <ahmed.amin@scilifelab.se>
2024-10-11 00:34:34 +0200

    Remove retry logic from Store session creation

--- genotype_api/database/store.py
+++ genotype_api/database/store.py
@@ -2,11 +2,8 @@
 
 from typing import AsyncGenerator
 
-from sqlalchemy.exc import OperationalError
 from sqlalchemy.ext.asyncio import AsyncSession
-from tenacity import retry, retry_if_exception_type, stop_after_attempt, wait_fixed
 
-from genotype_api.config import settings
 from genotype_api.database.crud.create import CreateHandler
 from genotype_api.database.crud.delete import DeleteHandler
 from genotype_api.database.crud.read import ReadHandler
@@ -29,16 +26,10 @@ class Store(
         UpdateHandler.__init__(self, session)
 
     @classmethod
-    @retry(
-        stop=stop_after_attempt(settings.max_retries),
-        wait=wait_fixed(settings.retry_delay),
-        retry=retry_if_exception_type(OperationalError),
-        reraise=True,
-    )
     async def create(cls) -> "Store":
         """Asynchronously create and return a Store instance with a session."""
-        async with get_session() as session:  # Correctly use async context manager
-            return cls(session)  # Return a Store instance with the session
+        async with get_session() as session:
+            return cls(session)
 
 
 async def get_store() -> AsyncGenerator[Store, None]:
