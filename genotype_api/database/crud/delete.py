43d86818bec9aa51318980e1a20ecd07945cdfe2
ahdamin <ahmed.amin@scilifelab.se>
2024-10-11 17:32:17 +0200

    Make delete awaitable

--- genotype_api/database/crud/delete.py
+++ genotype_api/database/crud/delete.py
@@ -11,19 +11,19 @@ LOG = logging.getLogger(__name__)
 class DeleteHandler(BaseHandler):
 
     async def delete_analysis(self, analysis: Analysis) -> None:
-        self.session.delete(analysis)
+        await self.session.delete(analysis)
         await self.session.commit()
 
     async def delete_plate(self, plate: Plate) -> None:
-        self.session.delete(plate)
+        await self.session.delete(plate)
         await self.session.commit()
 
     async def delete_sample(self, sample: Sample) -> None:
-        self.session.delete(sample)
+        await self.session.delete(sample)
         await self.session.commit()
 
     async def delete_user(self, user: User) -> None:
-        self.session.delete(user)
+        await self.session.delete(user)
         await self.session.commit()
 
     async def delete_snps(self) -> int:
@@ -31,6 +31,6 @@ class DeleteHandler(BaseHandler):
         snps: list[SNP] = await self.fetch_all_rows(query)
         count: int = len(snps)
         for snp in snps:
-            self.session.delete(snp)
+            await self.session.delete(snp)
         await self.session.commit()
         return count
